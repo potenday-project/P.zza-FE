6f90aec8dc71bc6e742f4b86a4b2831955b39e85
Wlfjd <leejiwon813@naver.com>
2023-12-14 23:43:45 +0900

    💄 Design:  TopicList.jsx 버튼 추가

--- src/pages/create-project/TopicList.jsx
+++ src/pages/create-project/TopicList.jsx
@@ -1,6 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { TopNav } from '../../components/TopNav/TopNav';
+import { useState } from 'react';
+import { Button } from '../../components/button/Button';
 
 export function TopicList() {
+  const [isClick, setIsClick] = useState(false);
+  const navigate = useNavigate();
   return (
     <>
       <TopNav>프로젝트 주제</TopNav>
@@ -11,7 +16,26 @@ export function TopicList() {
           추천 프로젝트 주제입니다
         </h3>
         <p>주제 선정에 도움이 되셨으면 좋겠어요!</p>
-        <div></div>
+        <div className="button-container">
+          <Button
+            className={`half-button ${isClick ? 'clicked' : ''}`}
+            onClick={() => {
+              navigate(-1);
+              setIsClick(true);
+            }}
+          >
+            이전
+          </Button>
+          <Button
+            className={`half-button ${isClick ? 'clicked' : ''}`}
+            onClick={() => {
+              navigate(-1);
+              setIsClick(true);
+            }}
+          >
+            다음
+          </Button>
+        </div>
       </div>
     </>
   );
