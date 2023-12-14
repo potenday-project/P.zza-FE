ad8fa54ad474f925436241012da9e2a2e6f3dee2
Wlfjd <leejiwon813@naver.com>
2023-12-14 23:26:47 +0900

    ✨ Feat :  주제 키워드 받는 기능 구현

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import './Input.scss';
 
-export function Input({ children, value, placeholder, onChange }) {
+export function Input({ children, value, name, placeholder, onChange }) {
   const [input, setInput] = useState(value);
   const handleInputChange = (e) => {
     setInput(e.target.value);
-    onChange('portfolio_url', e.target.value);
+    onChange(name, e.target.value);
   };
+
   return (
     <div className="input-container">
       <div className="label">{children}</div>
       <div className="inputbox">
         <input
+          name={name}
           placeholder={placeholder}
           value={input}
           onChange={handleInputChange}
--- src/pages/create-project/TopicNo.jsx
+++ src/pages/create-project/TopicNo.jsx
@@ -1,24 +1,48 @@
+import React, { useState } from 'react';
 import { TopNav } from '../../components/TopNav/TopNav';
 import { Button } from '../../components/button/Button';
 import { Input } from '../../components/input/Input';
+import './TopicNo.scss';
 
-const NUM = [1, 2, 3, 4, 5];
+const INITIAL = Array.from({ length: 5 }, () => ({ recommendation: '' }));
+
 export function TopicNo() {
+  const [values, setValues] = useState(INITIAL);
+  const [isClick, setIsClick] = useState(false);
+
+  const handleChange = (index, value) => {
+    const newValues = [...values];
+    newValues[index] = { recommendation: value };
+    setValues(newValues);
+  };
+  console.log(values);
+
   return (
-    <>
+    <div>
       <TopNav>프로젝트 주제</TopNav>
-      <div>
+      <div className="topic-container">
         <h3>
           관심사를 키워드로 입력해보세요 <br /> AI가 주제를 추천해드립니다.
         </h3>
         <p>주제는 명사로 입력해주세요</p>
         <div>
-          {NUM.map((item) => (
-            <Input></Input>
+          {INITIAL.map((item, i) => (
+            <Input
+              key={i}
+              name="recommendation"
+              value={values[i].recommendation}
+              placeholder={`주제${1 + i}`}
+              onChange={(name, value) => handleChange(i, value)}
+            />
           ))}
         </div>
-        <Button></Button>
+        <Button
+          className={`button ${isClick ? 'clicked' : ''}`}
+          onClick={() => {}}
+        >
+          확인
+        </Button>
       </div>
-    </>
+    </div>
   );
 }
