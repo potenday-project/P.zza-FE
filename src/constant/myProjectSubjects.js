e054b8cbd67f6569f7c94f93edeec9deea9707e7
jinyoung <wlsud801@gmail.com>
2023-12-16 02:33:28 +0900

    ✨ Feat :  내 프로젝트 페이지 작성

---
+++ src/constant/myProjectSubjects.js
@@ -0,0 +1,7 @@
+export const MY_PROJECT_SUBJECTS = [
+  { name: '내 프로젝트', stateKey: 'inProgress' },
+  { name: '완료 프로젝트', stateKey: 'complete' },
+  { name: '거절된 프로젝트', stateKey: 'rejected' },
+  { name: '심사 중 프로젝트', stateKey: 'pending' },
+  { name: '중단 프로젝트', stateKey: 'stoped' },
+];
